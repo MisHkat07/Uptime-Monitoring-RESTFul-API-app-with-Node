a5721c668d10f04eb1742c7a03d65e91e2c02d48
MisHkat <mishkat606@gmail.com>
2022-04-03 01:11:13 +0600

    Token Create and Post

--- Assistants/utilities.js
+++ Assistants/utilities.js
@@ -23,5 +23,21 @@ utilities.hash = (str) => {
   }
   return false;
 };
+utilities.createRandomString = (strlegth) => {
+  let length = strlegth;
+  length = typeof strlegth === 'number' && strlegth > 0 ? strlegth : false;
+  if (length) {
+    const possibleCharacters = 'abcdefghijklmnopqrstuvwxyz1234567890';
+    let output = '';
+    for (let i = 1; i <= length; i += 1) {
+      const randomCharacter = possibleCharacters.charAt(
+        Math.floor(Math.random() * possibleCharacters.length)
+      );
+      output += randomCharacter;
+    }
+    return output;
+  }
+  return false;
+};
 
 module.exports = utilities;
